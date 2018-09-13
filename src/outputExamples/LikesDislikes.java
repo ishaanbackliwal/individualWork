f2e9b48b26c678d9d47eb57d2b3f9b7a8514738f
Ishaan Backliwal <ishbackliwal@gmail.com>
2018-09-13 14:03:56 -0500

    likes and dislikes

---
+++ src/outputExamples/LikesDislikes.java
@@ -0,0 +1,16 @@
+package outputExamples;
+
+public class LikesDislikes {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("\t Likes \t\t\t\t Dislikes");
+		System.out.println(" ");
+		System.out.println("1. \t Pasta \t\t\t\t Tests");
+		System.out.println("2. \t Music \t\t\t\t Baseball");
+		System.out.println("3. \t Ramen \t\t\t\t College Applications");
+		System.out.println("4. \t NYC \t\t\t\t Video Games");
+		System.out.println("5. \t Movies \t\t\t Mowing the Lawn");
+	}
+
+}
