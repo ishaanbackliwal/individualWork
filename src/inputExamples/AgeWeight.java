80bc2a4295f0e6d37aba6fbd5045162871b6fdc6
Ishaan Backliwal <ishbackliwal@gmail.com>
2018-09-20 13:30:11 -0500

    age and weight

---
+++ src/inputExamples/AgeWeight.java
@@ -0,0 +1,18 @@
+package inputExamples;
+import java.util.Scanner;
+public class AgeWeight {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int age = -999;
+		int weight = -999;
+		Scanner input = new Scanner(System.in);
+		System.out.println("What is your age? Enter and integer.");
+		age = input.nextInt();
+		System.out.println("You are " + age + " years old? Thats great! \n ");
+		System.out.println("How much do you weigh in pounds? Enter and integer.");
+		weight = input.nextInt();
+		System.out.println("You weigh " + weight + " pounds? Good for you.");
+	}
+
+}
