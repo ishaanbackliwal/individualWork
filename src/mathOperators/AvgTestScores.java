23c77e608c5b3dcfa5bfde0af29297b4b9536451
Ishaan Backliwal <ishbackliwal@gmail.com>
2018-09-26 14:03:50 -0500

    average of 3 test scores v2

--- src/mathOperators/AvgTestScores.java
+++ src/mathOperators/AvgTestScores.java
@@ -7,16 +7,16 @@ public class AvgTestScores {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner input = new Scanner(System.in);
-		int test1 = -999;
-		int test2 = -999;
-		int test3 = -999;
-		int average = -999;
+		double test1 = -999;
+		double test2 = -999;
+		double test3 = -999;
+		double average = -999;
 		System.out.println("Insert the percentage you got on your first test (integer value).");
-		test1 = input.nextInt();
+		test1 = input.nextDouble();
 		System.out.println("Insert the percentage you got on your second test (integer value).");
-		test2 = input.nextInt();
+		test2 = input.nextDouble();
 		System.out.println("Insert the percentage you got on your third test (integer value).");
-		test3 = input.nextInt();
+		test3 = input.nextDouble();
 		average = (test1 + test2 + test3) / 3;
 		System.out.println("\nTest 1: " + test1 + "%");
 		System.out.println("Test 2: " + test2 + "%");
