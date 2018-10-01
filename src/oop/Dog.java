cb94792071a3407167f3b66aab5964b4a63ec130
Ishaan Backliwal <ishbackliwal@gmail.com>
2018-10-01 15:02:23 -0500

    dog class

---
+++ src/oop/Dog.java
@@ -0,0 +1,16 @@
+package oop;
+
+public class Dog {
+
+	public Dog()
+	
+	{
+		
+	}
+	public void woof() {
+		System.out.println("woof");
+	}
+	public void attack() {
+		System.out.println("grrrrrrr");
+	}
+}
