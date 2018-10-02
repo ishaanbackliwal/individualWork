51271d9ab95043da3dc577ebd5fb2e429a95c208
Ishaan Backliwal <ishbackliwal@gmail.com>
2018-10-02 14:14:25 -0500

    test class for car (and car class)

--- src/oop/Car.java
+++ src/oop/Car.java
@@ -7,5 +7,13 @@ public class Car {
 	{
 
 	}
-
+	public void sayOdometer() {
+		System.out.println("The car has 12456374 miles on it");
+	}
+	public void sayFlat() {
+		System.out.println("You have a flat tire");
+	}
+	public void sayThirsty() {
+		System.out.println("No more gas. Get gas. Im outta gas.");
+	}
 }
