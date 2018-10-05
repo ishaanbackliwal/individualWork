efd9909135427d5b2248dc4cd940941badccd4df
Ishaan Backliwal <ishbackliwal@gmail.com>
2018-10-05 15:06:01 -0500

    changes to number class and test

--- src/oop/NumberTest.java
+++ src/oop/NumberTest.java
@@ -7,6 +7,12 @@ public class NumberTest {
 		digit.sayNumber(7.5);
 		digit.sayNumberPlus2(7.5);
 		digit.saySum(7.5, 2.5);
+		double result = digit.returnSquare(7.5);
+		System.out.println("The square of the value is " + result);
+		double area = digit.returnArea(7.5, 2.5);
+		System.out.println("The area of the rectangle is " + area);
+		double roundUp = digit.returnRoundUp(7.5);
+		System.out.println("The next largest integer to your value is " + roundUp);
 	}
 
 }
--- src/oop/Numbers.java
+++ src/oop/Numbers.java
@@ -6,16 +6,27 @@ public class Numbers {
 
 	}
 	public void sayNumber(double num) {
-		System.out.println("Your number is: " + num);
+		System.out.println("Your number is " + num);
 	}
 	public void sayNumberPlus2(double num) {
 		num = num + 2.0;
-		System.out.println("Your number plus two is: " + num);
+		System.out.println("Your number plus two is " + num);
 	}
 	public void saySum(double first, double second) {
 		double sum = -999.0;
 		sum = first + second;
 		System.out.println("You sent the method " + first + " and " + second + "\nThe sum of the numbers is: " + sum);
 	}
+	public double returnSquare(double value){
+		double square = Math.pow(value, 2);
+		return square;
+	}
+	public double returnArea(double length, double width){
+		double area = length * width;
+		return area;
+	}
+	public int returnRoundUp(double value){
+		int temp = (int)Math.ceil(value);
+		return temp;
+	}
 }
-	
