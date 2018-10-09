731771ba1514d02b7c1bb68141be22c42b630dfc
Ishaan Backliwal <ishbackliwal@gmail.com>
2018-10-09 15:02:53 -0500

    modifications to greeter class and test

--- src/oop/Greeter.java
+++ src/oop/Greeter.java
@@ -1,15 +1,21 @@
 package oop;
 
 public class Greeter {
-
-	public Greeter()
-
-	{
-
+	
+	private int age;
+	
+	public Greeter(){
+		age = 17;
 	}
 
 	public void sayHello() {
 		System.out.println("Hello");
+	}
+	public int getAge(){
+		return age;
+	}
+	public void setAge(int newAge){
+		age = newAge;
 	}
 
 }
