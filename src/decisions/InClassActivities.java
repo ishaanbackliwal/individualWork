a5204dbc9afcba3d1e565fc034f7474461d7b129
Unknown <ishbackliwal@gmail.com>
2018-10-16 15:07:30 -0500

    in class activities (test class)

---
+++ src/decisions/InClassActivities.java
@@ -0,0 +1,27 @@
+package decisions;
+
+import java.util.Scanner;
+
+public class InClassActivities {
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		
+		/* age
+		System.out.println("What is your age?");
+		int age = input.nextInt(); 
+		System.out.println(age==16);
+		*/
+		
+		/* height and shoe size
+		System.out.println("What is your height in inches?");
+		double height = input.nextDouble();
+		System.out.println("What is your shoe size?");
+		double shoeSize = input.nextDouble();
+		System.out.println(height+shoeSize>=26);
+		*/
+		
+		
+	}
+
+}
