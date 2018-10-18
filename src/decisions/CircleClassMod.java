e1911a6d5d8c3859a4154c021e1abd8db99a471b
Ishaan Backliwal <ishbackliwal@gmail.com>
2018-10-18 09:46:52 -0500

    circle class modification (idk what to do w this)

---
+++ src/decisions/CircleClassMod.java
@@ -0,0 +1,25 @@
+package decisions;
+
+public class CircleClassMod {
+	
+	private double radius;
+	
+	public CircleClassMod() {
+		radius = 10.0;
+	}
+	public CircleClassMod(double newRadius) {
+		radius = newRadius;
+	}
+	public double getRadius() {
+		return radius;
+	}
+	public double perimeter() {
+		double circ = 2.0 * radius * Math.PI;
+		return circ;
+	}
+	public double area() {
+		double ar = radius * radius * Math.PI;
+		return ar;
+	}
+
+}
