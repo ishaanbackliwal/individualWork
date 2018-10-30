6265543f137238371a357960aad47fd1d01b13b8
Ishaan Backliwal <ishbackliwal@gmail.com>
2018-10-30 15:08:39 -0500

    num output (unfinished)

---
+++ src/decisions/NumOutput.java
@@ -0,0 +1,15 @@
+package decisions;
+
+public class NumOutput {
+
+	public static void main(String[] args) {
+		int counter = 0;
+		int row = 0;
+		for (counter = 1; counter <= 5; counter++) {
+			for (row = 1; row <= ) {
+				System.out.print();
+			}
+		}
+	}
+
+}
