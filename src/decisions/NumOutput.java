d6cfeaebde2f9d0a5d0be4eadb46222ff6a467ca
Unknown <ishbackliwal@gmail.com>
2018-10-31 15:01:05 -0500

    number output finished

--- src/decisions/NumOutput.java
+++ src/decisions/NumOutput.java
@@ -4,12 +4,13 @@ public class NumOutput {
 
 	public static void main(String[] args) {
 		int counter = 0;
-		int row = 0;
+		int num = 0;
 		for (counter = 1; counter <= 5; counter++) {
-			for (row = 1; row <= ) {
-				System.out.print();
+			for (num = counter; num <= 5; num++) {
+				System.out.print(num + " ");
 			}
+			System.out.println(counter);
 		}
 	}
-
 }
+
